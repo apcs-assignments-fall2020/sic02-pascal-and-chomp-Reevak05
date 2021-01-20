204c44ca16d2a508b261e1c9d3cfc6d81caee445
Reevak05 <kaveergera@gmail.com>
2021-01-20 09:10:06 -0800

    add pascalTriangle() functionality

--- src/main/java/Pascal.java
+++ src/main/java/Pascal.java
@@ -8,6 +8,21 @@ public class Pascal
 {
     public static int[][] pascalTriangle(int n) {
         // YOUR CODE HERE
-        return null;
+        //create 2D array
+        int[][] triangle = new int[n][n];
+
+        //set all 2D array values to 0
+        for (int[] row: triangle) for (int value: row) value = 0;
+
+        //set top left value to one
+        triangle[0][0] = 1;
+
+        //set values of each row based on the value of the previous row
+        for (int row = 1; row<triangle.length; row++) {
+            triangle[row][0] = 1;
+            triangle[row][row-1] = 1;
+            for (int column = 1; column<triangle[0].length; column++) triangle[row][column] = triangle[row-1][column-1] + triangle[row-1][column];
+        }
+    return triangle;
     }
 }
